82c8ffb45de74184b6e7b6e57b175d8e9ee6fb1b
unkwon <bruno_shenn@hotmail.com>
2014-04-24 09:45:41 -0300

    feito algumas alterações no wp-config

--- wp-config.php
+++ wp-config.php
@@ -1,37 +1,58 @@
 <?php
 /** 
- * As configurações básicas do WordPress.
- *
- * Esse arquivo contém as seguintes configurações: configurações de MySQL, Prefixo de Tabelas,
- * Chaves secretas, Idioma do WordPress, e ABSPATH. Você pode encontrar mais informações
- * visitando {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. Você pode obter as configurações de MySQL de seu servidor de hospedagem.
- *
- * Esse arquivo é usado pelo script ed criação wp-config.php durante a
- * instalação. Você não precisa usar o site, você pode apenas salvar esse arquivo
- * como "wp-config.php" e preencher os valores.
- *
- * @package WordPress
- */
+ * As configurações básicas do WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
 
-// ** Configurações do MySQL - Você pode pegar essas informações com o serviço de hospedagem ** //
-/** O nome do banco de dados do WordPress */
-define('DB_NAME', 'bsantana');
+$__hasconfigured = false;
+$__configuration_files = array(
+	'localhost' => "wp-config-localhost.php",
+	'qa' => "wp-config-qa.php",
+	'live' => "wp-config-live.php",
+);
+foreach ($__configuration_files as $enviroment => $file) {
+	if( file_exists( ABSPATH . $file ) && !$__hasconfigured) {
+		require_once( ABSPATH . $file );
 
-/** Usuário do banco de dados MySQL */
-define('DB_USER', 'root');
+		$__hasconfigured = true;
 
-/** Senha do banco de dados MySQL */
-define('DB_PASSWORD', 'newlii123');
+		define('WP_ENV', $enviroment);
+	}
+}
 
-/** nome do host do MySQL */
-define('DB_HOST', 'localhost');
+if( !$__hasconfigured ) {
+	/**
+	 * Esse arquivo contém as seguintes configurações: configurações de MySQL, Prefixo de Tabelas,
+	 * Chaves secretas, Idioma do WordPress, e ABSPATH. Você pode encontrar mais informações
+	 * visitando {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+	 * wp-config.php} Codex page. Você pode obter as configurações de MySQL de seu servidor de hospedagem.
+	 *
+	 * Esse arquivo é usado pelo script ed criação wp-config.php durante a
+	 * instalação. Você não precisa usar o site, você pode apenas salvar esse arquivo
+	 * como "wp-config.php" e preencher os valores.
+	 *
+	 * @package WordPress
+	 */
 
-/** Conjunto de caracteres do banco de dados a ser usado na criação das tabelas. */
-define('DB_CHARSET', 'utf8');
+	// ** Configurações do MySQL - Você pode pegar essas informações com o serviço de hospedagem ** //
+	/** O nome do banco de dados do WordPress */
+	define('DB_NAME', 'bsantana');
 
-/** O tipo de collate do banco de dados. Não altere isso se tiver dúvidas. */
-define('DB_COLLATE', '');
+	/** Usuário do banco de dados MySQL */
+	define('DB_USER', 'root');
+
+	/** Senha do banco de dados MySQL */
+	define('DB_PASSWORD', 'newlii123');
+
+	/** nome do host do MySQL */
+	define('DB_HOST', 'localhost');
+
+	/** Conjunto de caracteres do banco de dados a ser usado na criação das tabelas. */
+	define('DB_CHARSET', 'utf8');
+
+	/** O tipo de collate do banco de dados. Não altere isso se tiver dúvidas. */
+	define('DB_COLLATE', '');
+}
 
 /**#@+
  * Chaves únicas de autenticação e salts.
