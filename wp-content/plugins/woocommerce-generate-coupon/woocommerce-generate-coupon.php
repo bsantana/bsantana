9151b98a85002345b4607191fdf1e4ca8086c960
bsantana <bruno_shenn@hotmail.com>
2017-07-18 14:52:07 -0300

    Salvando status

--- wp-content/plugins/woocommerce-generate-coupon/woocommerce-generate-coupon.php
+++ wp-content/plugins/woocommerce-generate-coupon/woocommerce-generate-coupon.php
@@ -37,7 +37,64 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	 $user = $order->get_user();
 	 print_r($user);
 	 echo 'separacaaaaaaaaaaaaaaaaaaaaao';
+	 echo '<br>';
 	 $products = $order->get_items();
+	 /*foreach ( $products as $item ) {
+  		// This will be a product
+  		echo 'from item:';
+  		$product = $order->get_product_from_item( $item );
+  		print_r($product);
+  
+  	echo 'get sku:';
+  		// This is the products SKU
+		$sku = $product->get_sku();
+		print_r($sku);
+		
+		echo 'qnty profissio:';
+		// This is the qty purchased
+		$qty = $item['qty'];
+		print_r($qty);
+		/*foreach ( $item as $item_data ) {
+			echo 'item release:';
+			echo '<pre>';
+			print_r($item_data);
+			echo $item_data;
+			echo '</pre>';
+		}*/
+		//$orderr = wc_get_order( $order_id );
+	  foreach ($order->get_items() as $item_id => $item_obj) {
+	  		echo 'aqui: ';
+	  		echo $order_id;
+	         $kua = $item_obj->get_meta_data();
+	         $data = wc_get_order_item_meta( $item_id, '_tmcartepo_data' );
+	         echo '<pre>';
+	          print_r($data);
+	          if ($data[0]['value'] == "Mensal") {
+	          	echo 'foi';
+	          	echo $data[2]['quantity'];
+	          }
+
+
+	  }
+
+	/*	echo 'item release:';
+		echo '<pre>';
+		print_r($item);
+		echo '</pre>';
+		
+		
+		echo 'line total:';
+		// Line item total cost including taxes and rounded
+		$total = $order->get_line_total( $item, true, true );
+		print_r($order->get_meta());
+		echo 'line subtotal:';
+		// Line item subtotal (before discounts)
+		$subtotal = $order->get_line_subtotal( $item, true, true );
+		print_r($order->get_meta_data());
+	}*/
+
+	 
+	 echo '<pre>';
 	 print_r($products);
 	 //$order->update_status( 'completed' );
 
